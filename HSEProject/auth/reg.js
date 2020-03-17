cc91d30999da9448b70511e4010e25eb821448ed
Tatiana Kostina <32322253+OSmile@users.noreply.github.com>
2020-03-17 14:12:35 +0300

    Add files via upload

---
+++ HSEProject/auth/reg.js
@@ -0,0 +1,10 @@
+var email = document.getElementById("email")
+var pass = document.getElementById("password")
+var btn = document.getElementById("reg")
+
+
+
+btn.onClick = function() {
+    alert(email)
+    alert(pass)
+}
