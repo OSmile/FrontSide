4ffa877fa1b852e86815f992c1fea406cec3c2cb
rolledm <rolledm@gmail.com>
2020-04-03 21:30:41 +0300

    Login/Register

--- HSEProject/auth/login.js
+++ HSEProject/auth/login.js
@@ -1,9 +1,21 @@
 window.onload = function() {
-    var email = document.getElementById("email")
-    var pass = document.getElementById("password")
-    var btn = document.getElementById("btn")
+    let btn = document.getElementById("btn")
 
-    btn.addEventListener("click", function() {
-        if (email && pass) alert("OK!")
+    btn.addEventListener("click", () => {
+        let login = document.getElementById("login").value
+        let pass = document.getElementById("password").value
+        let url = new this.URL("http://localhost:5000/user")
+        url.search = new this.URLSearchParams({
+            'login': login,
+            'password': pass
+        })
+        this.fetch(url)
+            .then(res => res.json())
+                .then(
+                    (result) => {
+                        this.localStorage.setItem("login", login)
+                        this.alert("Success")
+                    }
+                )
     })
 }
